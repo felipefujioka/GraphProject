282c47a7ce1a48214ae8d1654bc68cb42438616e
felipefujioka <felipefujioka@gmail.com>
2016-05-04 07:54:15 -0300

    errors trying to resolve angular

--- app/mainController.js
+++ app/mainController.js
@@ -1,35 +1,11 @@
 //public/mainController
-var friendsListModule = angular.module('friendsListModule', []);
+var friendsListModule = angular.module("friendsApp", []);
 
-function mainController($scope, $http) {
-    $scope.formData = {};
+friendsListModule.controller("mainController", function($scope, $http) {
+        $scope.formData = {};
 
-    // when landing on the page, get all todos and show them
-    $http.get('/api/friend')
-        .success(function(data) {
-            $scope.todos = data;
-            console.log(data);
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
-        });
-
-    // when submitting the add form, send the text to the node API
-    $scope.createTodo = function() {
-        $http.post('/api/friend', $scope.formData)
-            .success(function(data) {
-                $scope.formData = {}; // clear the form so our user is ready to enter another
-                $scope.todos = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
-    };
-
-    // delete a todo after checking it
-    $scope.deleteTodo = function(id) {
-        $http.delete('/api/friend/' + id)
+        // when landing on the page, get all todos and show them
+        $http.get('/api/friend')
             .success(function(data) {
                 $scope.todos = data;
                 console.log(data);
@@ -37,6 +13,31 @@ function mainController($scope, $http) {
             .error(function(data) {
                 console.log('Error: ' + data);
             });
-    };
 
+        // when submitting the add form, send the text to the node API
+        $scope.createTodo = function() {
+            $http.post('/api/friend', $scope.formData)
+                .success(function(data) {
+                    $scope.formData = {}; // clear the form so our user is ready to enter another
+                    $scope.todos = data;
+                    console.log(data);
+                })
+                .error(function(data) {
+                    console.log('Error: ' + data);
+                });
+        };
+
+        // delete a todo after checking it
+        $scope.deleteTodo = function(id) {
+            $http.delete('/api/friend/' + id)
+                .success(function(data) {
+                    $scope.todos = data;
+                    console.log(data);
+                })
+                .error(function(data) {
+                    console.log('Error: ' + data);
+                });
+        };
+
+    }
 }
