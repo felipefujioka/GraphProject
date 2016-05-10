4b3f126b5d8f5ea831079a8431543ab36ac5923d
felipefujioka <felipefujioka@gmail.com>
2016-05-09 23:47:08 -0300

    loggin in and retrieving photos from facebook

--- app/public/graphController.js
+++ app/public/graphController.js
@@ -4,30 +4,58 @@ var friendsApp = angular.module("graphApp", ['facebook'])
   FacebookProvider.init('1190530957644301');
 });
 
-friendsApp.controller('graphController',['$scope','$http', function($scope, $http, Facebook) {
+friendsApp.controller('graphController',['$scope','$http','Facebook', function($scope, $http, Facebook) {
+  $scope.loggingIn = '';
 
   $scope.login = function() {
+    $scope.loggingIn = 'is-active';
       // From now on you can use the Facebook service just as Facebook api says
       Facebook.login(function(response) {
         // Do something with response.
-        $scope.token = response.accessToken;
-        $scope.userId = response.userID;
+        if(response.status === 'connected') {
+          $scope.loggedIn = true;
+          $scope.token = response.authResponse.accessToken;
+          $scope.userId = response.authResponse.userID;
+          getUser();
+        } else {
+          $scope.loggedIn = false;
+        }
+        $scope.loggingIn = '';
       });
     };
 
-    $scope.getLoginStatus = function() {
+    var getLoginStatus = function() {
       Facebook.getLoginStatus(function(response) {
         if(response.status === 'connected') {
           $scope.loggedIn = true;
+          getUser();
         } else {
           $scope.loggedIn = false;
         }
       });
     };
 
-    $scope.me = function() {
+    var getUser = function() {
       Facebook.api('/me', function(response) {
         $scope.user = response;
       });
     };
+
+    $scope.retrievePhotos = function() {
+
+      $http({method: 'GET',
+  	        url: 'https://graph.facebook.com/me/photos?fields=source&limit=10',
+  	    	headers: {'Authorization': 'OAuth '+$scope.token}}).success(function(data) {
+  		         $scope.photos = data.data;
+  	  });
+
+  		$http({method: 'GET',
+  		        url: 'https://graph.facebook.com/me/picture?redirect=false',
+  		    	headers: {'Authorization': 'OAuth '+$scope.token}}).success(function(data) {
+  			$scope.picture = data.data;
+  		});
+
+    };
+
+    getLoginStatus();
 }]);
